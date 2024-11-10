82d6e5d6d27deabba07827d70c44ce6f77414349
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-11-10 15:29:04 -0600

    Ariel updated City in models, will see if raised error changes

--- models/city.py
+++ models/city.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python
 """ holds class City"""
 import models
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
+from models.base_model import Base
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+
+# Base = decalarative_base()
 
 
 class City(BaseModel, Base):
-    """Representation of city """
-    if models.storage_t == "db":
-        __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
-    else:
-        state_id = ""
-        name = ""
+    """
+    Rep of city
+    """
+
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="cities")
 
     def __init__(self, *args, **kwargs):
-        """initializes city"""
+        """
+        initializes city
+        """
         super().__init__(*args, **kwargs)
+        if models.storage_t != "db":
+            self.state_id = ""
+            self.name = ""
