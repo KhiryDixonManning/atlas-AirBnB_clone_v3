4cc8b7be21458d86651376304d156b1e40085d0c
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-11-10 15:38:14 -0600

    Ariel: caught amenity file up, needed back_populates to amenities from last project

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python
 """ holds class Amenity"""
 
-
-import models
+from models.place import place_amenity
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
     """Representation of Amenity, used same syntax in base_model"""
-    __tablename__ = 'amenities'
+    
+    __tablename__ = "amenities"
 
-    name = Column(String(128), nullable=False)
+    if BaseModel.storage_type == "db":
+        name = Column(String(128), nullable=False)
 
+        place_amenities = relationship(
+            "Place", secondary=place_amenity,
+            back_populates="amenities",
+            viewonly=False)
+    else:
+        name = ""
+        
     def __init__(self, *args, **kwargs):
         """initializes Amenity"""
         super().__init__(*args, **kwargs)
-        if models.storage_t != "db":
-            self.name = ""
