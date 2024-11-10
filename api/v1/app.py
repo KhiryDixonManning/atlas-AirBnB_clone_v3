fc4a91b2f7eb7ca16155f40158e6ff99dddcb141
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-11-09 19:46:17 -0600

    Ariel: module documentation seperated on three line for pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""This module defines a flask web app"""
+
+"""
+This module defines a flask web app
+"""
+
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
