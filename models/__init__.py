00e1d09c2f5e30403991ae5951d7084a588820cb
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-11-09 19:15:25 -0600

    defining storage_t in init file in models folder

---
+++ models/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+"""
+storage_t defined
+"""
+
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from os import getenv
+
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
+    storage = DBStorage()
+else:
+    storage = FileStorage()
+
+storage.reload()
