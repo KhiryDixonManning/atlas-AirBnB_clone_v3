a3735eae61d32a76778e2c6a54145eeb4a5a89f7
KhiryDixonManning <Manningkhiry@gmail.com>
2024-11-12 15:45:43 -0600

    changed init to fix circular import

--- models/__init__.py
+++ models/__init__.py
@@ -3,15 +3,11 @@
 storage_t defined
 """
 import os
-
-storage_t = os.environ.get('HBNB_TYPE_STORAGE', 'file')
-db = os.environ.get('HBNB_ENV', 'test')
-
+storage_t = os.getenv('HBNB_TYPE_STORAGE', 'file')
 if storage_t == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-
 storage.reload()
